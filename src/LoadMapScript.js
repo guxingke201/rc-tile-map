0dabe7ab2b4fda3ab8b22a0b7e0e75af39c2da21
彭星 <666595@nd.com>
2018-01-04 16:33:00 +0800

    升级为3.0

--- src/LoadMapScript.js
+++ src/LoadMapScript.js
@@ -1,6 +1,6 @@
 export default function (appKey, callbackName) {
   var script = document.createElement('script')
   script.type = 'text/javascript'
-  script.src = `//api.map.baidu.com/api?v=2.0&ak=${appKey}&callback=${callbackName}`
+  script.src = `//api.map.baidu.com/api?v=3.0&ak=${appKey}&callback=${callbackName}`
   document.body.appendChild(script)
 }
